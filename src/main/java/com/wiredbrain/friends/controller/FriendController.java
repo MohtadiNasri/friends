6ed48b4b4f44ba8ddaa5920009ccf44cd375ea2a
nasrimohtadi@gmail.com <Awkward@009>
2020-04-29 04:07:06 +0200

    Friend Controller after inserting values to database insert into friend values(null, 'Ada', 'Lovelace');

--- src/main/java/com/wiredbrain/friends/controller/FriendController.java
+++ src/main/java/com/wiredbrain/friends/controller/FriendController.java
@@ -1,13 +1,34 @@
 package com.wiredbrain.friends.controller;
 
+import com.wiredbrain.friends.model.Friend;
 import com.wiredbrain.friends.services.FriendService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class FriendController {
 
     @Autowired
     FriendService friendService;
+
+    @PostMapping("/friend")
+    Friend create(@RequestBody Friend friend){
+        return friendService.save(friend);
+    }
+
+    @GetMapping("/friend")
+    Iterable<Friend> read(){
+        return friendService.findAll();
+    }
+
+    @PutMapping("/friend")
+    Friend update(@RequestBody Friend friend){
+        return friendService.save(friend);
+    }
+
+    @DeleteMapping("/friend/{id}")
+    void delete(@PathVariable Integer id){
+        friendService.deleteById(id);
+    }
 
 }
