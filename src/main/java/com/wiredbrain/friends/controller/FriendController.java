8e3c01b78d31f2eb6097efdeee87a13b2bd4e4ee
nasrimohtadi@gmail.com <Awkward@009>
2020-04-29 04:33:57 +0200

    Added find by Query Service and updated the controller

--- src/main/java/com/wiredbrain/friends/controller/FriendController.java
+++ src/main/java/com/wiredbrain/friends/controller/FriendController.java
@@ -5,6 +5,8 @@ import com.wiredbrain.friends.services.FriendService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
+
 @RestController
 public class FriendController {
 
@@ -30,5 +32,24 @@ public class FriendController {
     void delete(@PathVariable Integer id){
         friendService.deleteById(id);
     }
+
+    @GetMapping("/friend/{id}")
+    Optional<Friend> findById(@PathVariable Integer id){
+        return friendService.findById(id);
+    }
+
+    @GetMapping("/friend/search")
+    Iterable<Friend> findByQuery(@RequestParam(value = "first", required = false)String firstName,
+                                 @RequestParam(value = "last", required = false)String lastName){
+        if(firstName!=null && lastName!=null)
+            return friendService.findByFirstNameAndLastName(firstName, lastName);
+        else if(firstName!=null)
+            return friendService.findByFirstName(firstName);
+        else if(lastName!=null)
+            return friendService.findByLastName(lastName);
+        else
+            return friendService.findAll();
+    }
+
 
 }
--- src/main/java/com/wiredbrain/friends/services/FriendService.java
+++ src/main/java/com/wiredbrain/friends/services/FriendService.java
@@ -5,4 +5,9 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface FriendService extends CrudRepository<Friend,Integer> {
 
+    Iterable<Friend> findByFirstNameAndLastName(String firstName, String lastName);
+    Iterable<Friend> findByFirstName(String firstName);
+    Iterable<Friend> findByLastName(String lastName);
+
+
 }
